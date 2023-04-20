4066042032c85c20aebfdfcad0aab8292f4903b7
Mithun Kamath <callmekatootie@gmail.com>
2023-04-20 13:06:37 +0530

    housekeeping

--- blocks/index.tsx
+++ blocks/index.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import * as styles from "react-syntax-highlighter/dist/esm/styles/prism";
 
+// Supported stylesheets from Prism
+// https://github.com/react-syntax-highlighter/react-syntax-highlighter/blob/HEAD/AVAILABLE_STYLES_PRISM.MD
 const availableStylesheets = [
   "coy",
   "dark",
@@ -49,15 +51,18 @@ const availableStylesheets = [
   "vscDarkPlus",
   "xonokai",
   "zTouch",
-];
+].sort((a, b) => a.localeCompare(b));
 
 export default function ExampleFileBlock(props: FileBlockProps) {
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [currentStyle, setCurrentStyle] = useState(availableStylesheets[selectedIndex]);
+  const oneDarkIndex = availableStylesheets.findIndex((a) => a === "prism");
+  const [selectedIndex, setSelectedIndex] = useState(oneDarkIndex);
+  const [currentStyle, setCurrentStyle] = useState(
+    availableStylesheets[selectedIndex]
+  );
   const { context, content } = props;
 
   useEffect(() => {
-    setCurrentStyle(availableStylesheets[selectedIndex])
+    setCurrentStyle(availableStylesheets[selectedIndex]);
   }, [selectedIndex]);
 
   const language = getLanguageFromFilename(context.path).toLowerCase();
@@ -66,7 +71,8 @@ export default function ExampleFileBlock(props: FileBlockProps) {
     return (
       <Box p={4}>
         <Flash variant="danger">
-          We could not identify the language in the file. It's contents are displayed as-is for now.
+          We could not identify the language in the file. It's contents are
+          displayed as-is for now.
         </Flash>
         <Box
           marginTop={5}
@@ -108,32 +114,24 @@ export default function ExampleFileBlock(props: FileBlockProps) {
           </Box>
           <Box>
             <ActionMenu>
-              <ActionMenu.Button
-                aria-label="Select theme"
-              >
+              <ActionMenu.Button aria-label="Select theme">
                 Theme: {availableStylesheets[selectedIndex]}
               </ActionMenu.Button>
-              <ActionMenu.Overlay
-                maxHeight="medium"
-              >
-                <ActionList
-                  selectionVariant="single"
-                >
-                  {
-                    availableStylesheets.map((s, i) => (
-                      <ActionList.Item
-                        key={s}
-                        selected={i === selectedIndex}
-                        onSelect={() => setSelectedIndex(i)}
-                      >
-                        {s}
-                      </ActionList.Item>
-                    ))
-                  }
+              <ActionMenu.Overlay maxHeight="medium" sx={{ overflow: "auto" }}>
+                <ActionList selectionVariant="single">
+                  {availableStylesheets.map((s, i) => (
+                    <ActionList.Item
+                      key={s}
+                      selected={i === selectedIndex}
+                      onSelect={() => setSelectedIndex(i)}
+                    >
+                      {s}
+                    </ActionList.Item>
+                  ))}
                 </ActionList>
               </ActionMenu.Overlay>
             </ActionMenu>
-          </Box>          
+          </Box>
         </Box>
         <Box>
           <SyntaxHighlighter
